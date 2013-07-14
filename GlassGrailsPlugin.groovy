5e97d45a5842aefbd9c64920ba2905127c9fad8a
Daniel Kelleher <dankelleher@yahoo.com>
2013-07-15 01:52:22 +0200

    Cleaned up some TODOs on plugin descriptor

--- GlassGrailsPlugin.groovy
+++ GlassGrailsPlugin.groovy
@@ -3,11 +3,9 @@ import org.seethrough.glass.MirrorClient
 class GlassGrailsPlugin {
 	def groupId = "org.seethrough.glass"
 	
-    // the plugin version
     def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
-    // resources that are excluded from plugin packaging
+
     def pluginExcludes = [
         "grails-app/views/error.gsp"
     ]
@@ -37,7 +35,6 @@ Subscribing to notifications from Glass, triggered by user actions.
 		]
 
     def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
     }
 
     def doWithSpring = {
@@ -47,28 +44,20 @@ Subscribing to notifications from Glass, triggered by user actions.
     }
 
     def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
 		def config = application.mergedConfig.grails.plugin.glass
 		MirrorClient.APP_NAME = config.appname
 		MirrorClient.IMAGE_URL = config.imageurl
     }
 
     def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
     }
 
     def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-		println "Config changed"
 		def config = application.mergedConfig.grails.plugin.glass
 		MirrorClient.APP_NAME = config.appname
 		MirrorClient.IMAGE_URL = config.imageurl
     }
 
     def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
     }
 }
