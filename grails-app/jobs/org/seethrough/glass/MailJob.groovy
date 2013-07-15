d8828452983118f59ced55aec004d0dcdc436a21
Burt Beckwith <burt@burtbeckwith.com>
2013-07-15 02:13:29 -0400

    fixed package<->directory structure

--- grails-app/jobs/org/seethrough/glass/MailJob.groovy
+++ grails-app/jobs/org/seethrough/glass/MailJob.groovy
@@ -2,12 +2,12 @@ package org.seethrough.glass
 
 class MailJob {
 	static triggers = {
-	  simple repeatInterval: 15000l // execute job once in 15 seconds
+		simple repeatInterval: 15000l // execute job once in 15 seconds
 	}
-	
+
 	def mailService
 
-	def execute() {
+	void execute() {
 		mailService.readMail()
 	}
 }
